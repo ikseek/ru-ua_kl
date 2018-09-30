64f82bc977e25bc60c2296bfcb5f276ec3bb7d9b
Igor Kozyrenko <igor@unity3d.com>
2018-09-30 14:16:05 +0300

    Install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+mkdir -p ~/Library/Keyboard\ Layouts/ && cd ~/Library/Keyboard\ Layouts/
+curl -LO https://raw.githubusercontent.com/ikseek/ru-ua_kl/master/Russian-Ukrainian.keylayout
+curl -LO https://raw.githubusercontent.com/ikseek/ru-ua_kl/master/Russian-Ukrainian.icns
